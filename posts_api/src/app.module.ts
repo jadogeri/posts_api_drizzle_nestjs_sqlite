09d0ba8894d882fdde911a01e6af601bbac98dc1
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-05-15 09:34:17 -0500

    Added user and post resources

--- posts_api/src/app.module.ts
+++ posts_api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostModule, UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
